0af3f4b37721c5f5138141c40e2e838dd8432999
ZXCVBNM <95721223+ZXCVBNM07@users.noreply.github.com>
2024-04-09 23:52:00 +0200

    Update offsetsPY.py

--- offsetsPY.py
+++ offsetsPY.py
@@ -3,11 +3,8 @@ Generated using https://github.com/a2x/cs2-dumper
 Sat, 16 Mar 2024 00:42:38 +0000
 '''
 class client_dll: # client.dll
-    m_Handle = 0x28 # ModelConfigHandle_t
-    m_Name = 0x30 # CUtlSymbolLarge
-    m_AssociatedEntities = 0x38 # C_NetworkUtlVectorBase<CHandle<C_BaseModelEntity>>
-    m_AssociatedEntityNames = 0x50 # C_NetworkUtlVectorBase<CUtlSymbolLarge>
-    dwEntityList = 0x18C2D58
+    dwCSGOInput = 0x192A5B0
+    dwEntityList = 0x18C1DB8
     dwForceAttack = 0x1730020
     dwForceAttack2 = 0x17300B0
     dwForceBackward = 0x17302F0
@@ -16,34 +13,31 @@ class client_dll: # client.dll
     dwForceJump = 0x1730530
     dwForceLeft = 0x1730380
     dwForceRight = 0x1730410
-    dwGameEntitySystem = 0x19E0790
+    dwGameEntitySystem = 0x19DF750
     dwGameEntitySystem_getHighestEntityIndex = 0x1510
-    dwGameRules = 0x191FCA0
-    dwGlobalVars = 0x172ABA0
-    dwGlowManager = 0x19200C0
+    dwGameRules = 0x191EC70
+    dwGlobalVars = 0x1729BA0
+    dwGlowManager = 0x191F090
     dwInterfaceLinkList = 0x1A118D8
-    dwLocalPlayerController = 0x1912578
-    dwLocalPlayerPawn = 0x17371A8
-    dwPlantedC4 = 0x1928AD8
-    dwPrediction = 0x1737070
-    dwSensitivity = 0x19209E8
+    dwLocalPlayerController = 0x1911578
+    dwLocalPlayerPawn = 0x17361E8
+    dwPlantedC4 = 0x1927A68
+    dwPrediction = 0x17360B0
+    dwSensitivity = 0x191F9B8
     dwSensitivity_sensitivity = 0x40
-    dwViewAngles = 0x19309B0
-    dwViewMatrix = 0x19241A0
-    dwViewRender = 0x1924A20
+    dwViewAngles = 0x192F940
+    dwViewMatrix = 0x19231B0
+    dwViewRender = 0x19239B0
 
 class engine2_dll: # engine2.dll
-    dwBuildNumber = 0x513574
+    dwBuildNumber = 0x515564
     dwNetworkGameClient_deltaTick = 0x258
-    dwNetworkGameClient = 0x512AC8
+    dwNetworkGameClient = 0x514AC8
     dwNetworkGameClient_getLocalPlayer = 0xF0
     dwNetworkGameClient_maxClients = 0x250
     dwNetworkGameClient_signOnState = 0x240
-    dwWindowHeight = 0x5CBC64
-    dwWindowWidth = 0x5CBC60
-
-class game_info: # Some additional information about the game at dump time
-    buildNumber = 0x36AD # Game build number
+    dwWindowHeight = 0x5CDD14
+    dwWindowWidth = 0x5CDD10
 
 class inputsystem_dll: # inputsystem.dll
     dwInputSystem = 0x367A0
@@ -51,3 +45,4 @@ class inputsystem_dll: # inputsystem.dll
 class matchmaking_dll: # matchmaking.dll
     dwGameTypes = 0x1D21E0
     dwGameTypes_mapName = 0x1D2300
+    
