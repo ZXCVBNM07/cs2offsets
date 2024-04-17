9b20a6ae53ab991e491cbece7a07923cb8dd0387
ZXCVBNM <95721223+ZXCVBNM07@users.noreply.github.com>
2024-04-17 20:44:00 +0200

    Update offsetsPY.py

--- offsetsPY.py
+++ offsetsPY.py
@@ -3,36 +3,28 @@ Generated using https://github.com/a2x/cs2-dumper
 Sat, 16 Mar 2024 00:42:38 +0000
 '''
 class client_dll: # client.dll
-    dwCSGOInput = 0x192A5B0
-    dwEntityList = 0x18C1DB8
-    dwForceAttack = 0x172F060
-    dwForceAttack2 = 0x172F0F0
-    dwForceBackward = 0x172F330
-    dwForceCrouch = 0x172F600
-    dwForceForward = 0x172F2A0
-    dwForceJump = 0x172F570
-    dwForceLeft = 0x172F3C0
-    dwForceRight = 0x172F450
-    dwGameEntitySystem = 0x19DF750
+    dwCSGOInput = 0x192A5E0
+    dwEntityList = 0x18C1DE8
+    dwGameEntitySystem = 0x19DF790
     dwGameEntitySystem_getHighestEntityIndex = 0x1510
-    dwGameRules = 0x191EC70
+    dwGameRules = 0x191ECA0
     dwGlobalVars = 0x1729BA0
-    dwGlowManager = 0x191F090
-    dwInterfaceLinkList = 0x1A118D8
-    dwLocalPlayerController = 0x1911578
+    dwGlowManager = 0x191F0C0
+    dwLocalPlayerController = 0x19115A8
     dwLocalPlayerPawn = 0x17361E8
-    dwPlantedC4 = 0x1927A68
+    dwPlantedC4 = 0x1927A98
     dwPrediction = 0x17360B0
-    dwSensitivity = 0x191F9B8
+    dwSensitivity = 0x191F9E8
     dwSensitivity_sensitivity = 0x40
-    dwViewAngles = 0x192F940
-    dwViewMatrix = 0x19231B0
-    dwViewRender = 0x19239B0
+    dwViewAngles = 0x192F970
+    dwViewMatrix = 0x19231E0
+    dwViewRender = 0x19239E0
+    dwWeaponC4 = 0x18C5240
 
 class engine2_dll: # engine2.dll
     dwBuildNumber = 0x515564
-    dwNetworkGameClient_deltaTick = 0x258
     dwNetworkGameClient = 0x514AC8
+    dwNetworkGameClient_deltaTick = 0x258
     dwNetworkGameClient_getLocalPlayer = 0xF0
     dwNetworkGameClient_maxClients = 0x250
     dwNetworkGameClient_signOnState = 0x240
@@ -45,4 +37,15 @@ class inputsystem_dll: # inputsystem.dll
 class matchmaking_dll: # matchmaking.dll
     dwGameTypes = 0x1D21E0
     dwGameTypes_mapName = 0x1D2300
+
+class Buttons:
+    dwForceAttack = 0x172F060
+    dwForceAttack2 = 0x172F0F0
+    dwForceBackward = 0x172F330
+    dwForceCrouch = 0x172F600
+    dwForceForward = 0x172F2A0
+    dwForceJump = 0x172F570
+    dwForceLeft = 0x172F3C0
+    dwForceRight = 0x172F450
+    
     
