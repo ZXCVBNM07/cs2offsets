6bf3b8351d3b7ec23ed6d41dcdcf288137fcca19
ZXCVBNM <95721223+ZXCVBNM07@users.noreply.github.com>
2024-06-17 00:06:42 +0200

    Update offsetsPY.py

--- offsetsPY.py
+++ offsetsPY.py
@@ -2,49 +2,49 @@
 Generated using https://github.com/a2x/cs2-dumper
 '''
 class client_dll: # client.dll
-    dwCSGOInput = 0x192A5E0
-    dwEntityList = 0x18C1DE8
-    dwGameEntitySystem = 0x19DF790
+    dwCSGOInput = 0x1A1E1B0
+    dwEntityList = 0x19B49B8
+    dwGameEntitySystem = 0x1AD38C8
     dwGameEntitySystem_getHighestEntityIndex = 0x1510
-    dwGameRules = 0x191ECA0
-    dwGlobalVars = 0x1729BA0
-    dwGlowManager = 0x191F0C0
-    dwLocalPlayerController = 0x19115A8
-    dwLocalPlayerPawn = 0x17361E8
-    dwPlantedC4 = 0x1927A98
-    dwPrediction = 0x17360B0
-    dwSensitivity = 0x191F9E8
+    dwGameRules = 0x1A12418
+    dwGlobalVars = 0x180E4E0
+    dwGlowManager = 0x1A11B30
+    dwLocalPlayerController = 0x1A04768
+    dwLocalPlayerPawn = 0x181A998
+    dwPlantedC4 = 0x1A1B528
+    dwPrediction = 0x181A850
+    dwSensitivity = 0x1A13258
     dwSensitivity_sensitivity = 0x40
-    dwViewAngles = 0x192F970
-    dwViewMatrix = 0x19231E0
-    dwViewRender = 0x19239E0
-    dwWeaponC4 = 0x18C5240
+    dwViewAngles = 0x1A235C8
+    dwViewMatrix = 0x1A16A70
+    dwViewRender = 0x1A17388
+    dwWeaponC4 = 0x19B86F0
 
 class engine2_dll: # engine2.dll
-    dwBuildNumber = 0x515564
-    dwNetworkGameClient = 0x514AC8
-    dwNetworkGameClient_deltaTick = 0x258
+    dwBuildNumber = 0x52F834
+    dwNetworkGameClient = 0x52EBA0
+    dwNetworkGameClient_deltaTick = 0x278
     dwNetworkGameClient_getLocalPlayer = 0xF0
-    dwNetworkGameClient_maxClients = 0x250
-    dwNetworkGameClient_signOnState = 0x240
-    dwWindowHeight = 0x5CDD14
-    dwWindowWidth = 0x5CDD10
+    dwNetworkGameClient_maxClients = 0x270
+    dwNetworkGameClient_signOnState = 0x260
+    dwWindowHeight = 0x5F0454
+    dwWindowWidth = 0x5F0450
 
 class inputsystem_dll: # inputsystem.dll
-    dwInputSystem = 0x367A0
+    dwInputSystem = 0x387F0
 
 class matchmaking_dll: # matchmaking.dll
-    dwGameTypes = 0x1D21E0
-    dwGameTypes_mapName = 0x1D2300
+    dwGameTypes = 0x1A41C0
+    dwGameTypes_mapName = 0x1A42E0
 
 class buttons:
-    dwForceAttack = 0x172F060
-    dwForceAttack2 = 0x172F0F0
-    dwForceBackward = 0x172F330
-    dwForceCrouch = 0x172F600
-    dwForceForward = 0x172F2A0
-    dwForceJump = 0x172F570
-    dwForceLeft = 0x172F3C0
-    dwForceRight = 0x172F450
+    dwForceAttack = 0x1813100
+    dwForceAttack2 = 0x1813190
+    dwForceBackward = 0x18133D0
+    dwForceCrouch = 0x18136A0
+    dwForceForward = 0x1813340
+    dwForceJump = 0x1813610
+    dwForceLeft = 0x1813460
+    dwForceRight = 0x18134F0
     
     
