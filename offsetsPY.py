ce69996b65b0656fdf4563d9b72192b188c0f8e5
ZXCVBNM <95721223+ZXCVBNM07@users.noreply.github.com>
2025-01-18 02:59:22 +0100

    Update offsetsPY.py

--- offsetsPY.py
+++ offsetsPY.py
@@ -2,23 +2,23 @@
 Generated using https://github.com/a2x/cs2-dumper
 '''
 class client_dll: # client.dll
-    dwCSGOInput = 0x1A1E430
-    dwEntityList = 0x19B49B8
-    dwGameEntitySystem = 0x1AD3888
-    dwGameEntitySystem_getHighestEntityIndex = 0x1510
-    dwGameRules = 0x1A12408
-    dwGlobalVars = 0x180E500
-    dwGlowManager = 0x1A11B20
-    dwLocalPlayerController = 0x1A04768
-    dwLocalPlayerPawn = 0x181A9B8
-    dwPlantedC4 = 0x1A1B7A8
-    dwPrediction = 0x181A870
-    dwSensitivity = 0x1A13248
+    dwCSGOInput = 0x1A8C460
+    dwEntityList = 0x1A176C8
+    dwGameEntitySystem = 0x1B3AF68
+    dwGameEntitySystem_highestEntityIndex = 0x20F0
+    dwGameRules = 0x1A7E048
+    dwGlobalVars = 0x185FAB8
+    dwGlowManager = 0x1A7D790
+    dwLocalPlayerController = 0x1A680D0
+    dwLocalPlayerPawn = 0x186BDF8
+    dwPlantedC4 = 0x1A86B10
+    dwPrediction = 0x186BC90
+    dwSensitivity = 0x1A7ED68
     dwSensitivity_sensitivity = 0x40
-    dwViewAngles = 0x1A23848
-    dwViewMatrix = 0x1A16A60
-    dwViewRender = 0x1A17378
-    dwWeaponC4 = 0x19B86F0
+    dwViewAngles = 0x1A8C830
+    dwViewMatrix = 0x1A82740
+    dwViewRender = 0x1A830B8
+    dwWeaponC4 = 0x1A1A950
 
 class engine2_dll: # engine2.dll
     dwBuildNumber = 0x52F834
@@ -38,13 +38,14 @@ class matchmaking_dll: # matchmaking.dll
     dwGameTypes_mapName = 0x1A42E0
 
 class buttons:
-    dwForceAttack = 0x1813120
-    dwForceAttack2 = 0x18131B0
-    dwForceBackward = 0x18133F0
-    dwForceCrouch = 0x18136C0
-    dwForceForward = 0x1813360
-    dwForceJump = 0x1813630
-    dwForceLeft = 0x1813480
-    dwForceRight = 0x1813510
+    dwForceAttack = 0x1864620
+    dwForceAttack2 = 0x18646B0
+    dwForceBackward = 0x18648F0
+    dwForceCrouch = 0x1864BC0
+    dwForceForward = 0x1864860
+    dwForceJump = 0x1864B30
+    dwForceLeft = 0x1864980
+    dwForceRight = 0x1864A10
+
     
     
